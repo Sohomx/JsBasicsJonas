b07426eea2f735b85342ba84308bd7d55bf95315
sohom <sohom377@gmail.com>
2021-06-03 23:26:08 +0530

    first commit

--- script.js
+++ script.js
@@ -404,3 +404,4 @@ const bill = 400;
 const tip = bill <= 300 && bill >= 50 ? bill * 0.15 : bill * 0.20;
 
 console.log(`The bill was ${bill}, the tip was the ${tip} and the total value was ${bill + tip}`);
+//end
